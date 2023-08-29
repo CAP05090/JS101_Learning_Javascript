f0cad7356b87485d143bb94325a35885b98c1548
CAP05090 <ksand7398@gmail.com>
2023-08-29 09:02:31 +0530

    Mathmatical Operator

---
+++ find_x.js
@@ -0,0 +1,6 @@
+function findX(X) {
+    // Write code here
+    console.log(X*3 + 10)
+    
+}
+findX(23)
---
+++ square_sum.js
@@ -0,0 +1,9 @@
+function squareAndSum(one, two, three) {
+  // Write code here
+  let sq_sum= one**2 + two**2 + three**2
+  console.log(sq_sum)
+}
+let a = 2
+let b = 5
+let c = 4
+squareAndSum(a,b,c)
